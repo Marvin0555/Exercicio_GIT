6048e1aa00b63a490daa195759025db8165c8a91
Marvin0555 <60906316+Marvin0555@users.noreply.github.com>
2023-11-22 00:48:01 -0400

    Branch_B
    Printa a data e hora.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+from datetime import datetime
+
+date_time = datetime.now()
+
 print("Aula 1 de Github")
 print(f"Soma de 1 + 1 = {1+1}")
+
+data_de_hoje = date_time.date()
+print(f"Data de hoje: {data_de_hoje}" )
+
+hora_de_agora = date_time.time()
+print(f"Hora de agora: {hora_de_agora}")
+
