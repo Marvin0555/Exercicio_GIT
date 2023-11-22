cccbf52f896d342c800443a4bdefb572e6d7feaa
Marvin0555 <60906316+Marvin0555@users.noreply.github.com>
2023-11-22 00:46:24 -0400

    Branch_A
    Print a Data de hoje

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from datetime import datetime
+
+date_de_agora = datetime.now()
+
 print("Aula 1 de Github")
 print(f"Soma de 1 + 1 = {1+1}")
+
+# print(data_hora_hoje)
+data_de_hoje = date_de_agora.date()
+print(f"Data de hoje: {data_de_hoje}" )
